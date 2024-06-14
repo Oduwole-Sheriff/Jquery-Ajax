4157f7c2cd38f14196853af119c233fa024143c6
Oduwole-Sheriff <oduwolesheriff1212@gmail.com>
2024-06-14 02:46:56 +0100

    added media root

--- jquery_ajax_app/models.py
+++ jquery_ajax_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from PIL import Image
 
 # Create your models here.
 class Post(models.Model):
     name = models.CharField(max_length=100)
     drink = models.CharField(max_length=100)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
--- jquery_ajax_app/static/jquery_ajax_app/js/main.js
+++ jquery_ajax_app/static/jquery_ajax_app/js/main.js
@@ -3,13 +3,15 @@ $(document).ready(function(){
     var $orders = $('#orders');
     var $name = $('#name');
     var $drink = $('#drink');
-
-    var orderTemplate = $('#order-template').html();
+    var $image = $('#image');
+    var $date = $('#date');
 
     var orderTemplate = "" +
     "<li data-id='{{id}}'>" +
     "<p><strong>Name:</strong> <span class='noEdit name'>{{name}}</span><input class='edit name' /></p>" +
     "<p><strong>Drink:</strong> <span class='noEdit drink'>{{drink}}</span><input class='edit drink' /></p>" +
+    "<img src='{{image}}'></img>" +
+    "<p><strong>Date:</strong> <span class='noEdit date'>{{date}}</span><input class='edit date' /></p>" +
     "<button data-id='{{id}}' class='remove'>X</button>" +
     "<button class='editOrder noEdit'>Edit</button>"+
     "<button data-id='{{id}}' class='saveEdit edit'>Save</button>"+
@@ -17,40 +19,43 @@ $(document).ready(function(){
     "</li>";
 
     function addOrder(order){
-        $orders.append(Mustache.render(orderTemplate, order)) ;
-        // $orders.append('<li>Name: '+order.name+', Drink: '+ order.drink +'</li>') 
+        $orders.append(Mustache.render(orderTemplate, order));
     }
-
 
     $.ajax({
         type: 'GET',
         url: '/api/post/',
         success: function(orders){
             $.each(orders, function(i, order){
-                addOrder(order)
-                // console.log(order)
-            })
+                addOrder(order);
+            });
         },
-        error: function() {
-            alert('error loading orders');
+        error: function(xhr, status, error) {
+            alert('Error loading orders');
         }
     });
 
     $('#add-order').click(function(){
-        var order = {
-            name: $name.val(),
-            drink: $drink.val(),
-        };
+        
+        event.preventDefault();
+
+        var formData = new FormData();
+        formData.append('name', $name.val());
+        formData.append('drink', $drink.val());
+        formData.append('image', $image[0].files[0]);
+        formData.append('date', $date.val());
 
         $.ajax({
             type: 'POST',
             url: '/api/post/',
-            data: order,
+            data: formData,
+            contentType: false,
+            processData: false,
             success: function(newOrder){
-                addOrder(newOrder)
+                addOrder(newOrder);
             },
-            error: function(){
-                alert('error saving order')
+            error: function(xhr, status, error){
+                alert('Error saving order');
             },
             error: function(xhr, status, error) {
                 var jsonResponse = JSON.parse(xhr.responseText);
@@ -62,12 +67,13 @@ $(document).ready(function(){
                     alert(errorMessage);
                 }
             }
-        })
+        });
     });
 
-    $orders.delegate('.remove', 'click', function(){
+    // Rest of your event handlers...
 
-        var dataId = $(this).attr('data-id')
+    $orders.on('click', '.remove', function(){
+        var dataId = $(this).attr('data-id');
         var $li = $(this).closest('li');
         $.ajax({
             type: 'DELETE',
@@ -75,35 +81,36 @@ $(document).ready(function(){
             data: { id: dataId },
             success: function() {
                 $li.fadeOut(300, function(){
-                    $('this').remove();
+                    $(this).remove();
                     console.log('Data deleted successfully');
-                })
+                });
             },
             error: function() {
-                console.error('Error deleting data:');
+                console.error('Error deleting data');
             }
-
         });
     });
 
-    $orders.delegate('.editOrder', 'click', function(){
+    $orders.on('click', '.editOrder', function(){
         var $li = $(this).closest('li');
         $li.find('input.name').val( $li.find('span.name').html() );
         $li.find('input.drink').val( $li.find('span.drink').html() );
         $li.addClass('edit');
-    })
+    });
 
-    $orders.delegate('.cancelEdit', 'click', function(){
+    $orders.on('click', '.cancelEdit', function(){
         $(this).closest('li').removeClass('edit');
-    })
+    });
 
-    $orders.delegate('.saveEdit', 'click', function(){
+    $orders.on('click', '.saveEdit', function(){
         var $li = $(this).closest('li');
-        var dataId = $(this).attr('data-id')
+        var dataId = $(this).attr('data-id');
         var order = {
             id: dataId,
             name: $li.find('input.name').val(),
-            drink: $li.find('input.drink').val()
+            drink: $li.find('input.drink').val(),
+            image: $li.find('input.image').val(),
+            date: $li.find('input.date').val()
         };
         $.ajax({
             type: 'PUT',
@@ -114,11 +121,139 @@ $(document).ready(function(){
                 $li.find('span.drink').html(order.drink);
                 $li.removeClass('edit');
             },
-            error: function() {
-                alert('Error updating data:', error);
+            error: function(xhr, status, error) {
+                alert('Error updating data: ' + error);
             }
-
         });
-    })
+    });
+}); 
+
+
+// $(document).ready(function(){
+
+//     var $orders = $('#orders');
+//     var $name = $('#name');
+//     var $drink = $('#drink');
+//     var $image = $('#image');
+//     var $date = $('#date');
+
+//     var orderTemplate = $('#order-template').html();
+
+//     var orderTemplate = "" +
+//     "<li data-id='{{id}}'>" +
+//     "<p><strong>Name:</strong> <span class='noEdit name'>{{name}}</span><input class='edit name' /></p>" +
+//     "<p><strong>Drink:</strong> <span class='noEdit drink'>{{drink}}</span><input class='edit drink' /></p>" +
+//     "<img src='{{image}}'></img>" +
+//     "<p><strong>Date:</strong> <span class='noEdit image'>{{date}}</span><input class='edit date' /></p>" +
+//     "<button data-id='{{id}}' class='remove'>X</button>" +
+//     "<button class='editOrder noEdit'>Edit</button>"+
+//     "<button data-id='{{id}}' class='saveEdit edit'>Save</button>"+
+//     "<button class='cancelEdit edit'>Cancel</button>"+
+//     "</li>";
+
+//     function addOrder(order){
+//         $orders.append(Mustache.render(orderTemplate, order)) ;
+//         // $orders.append('<li>Name: '+order.name+', Drink: '+ order.drink +'</li>') 
+//     }
+
+
+//     $.ajax({
+//         type: 'GET',
+//         url: '/api/post/',
+//         success: function(orders){
+//             $.each(orders, function(i, order){
+//                 addOrder(order)
+//                 // console.log(order)
+//             })
+//         },
+//         error: function() {
+//             alert('error loading orders');
+//         }
+//     });
+
+//     $('#add-order').click(function(){
+//         var order = {
+//             name: $name.val(),
+//             drink: $drink.val(),
+//         };
+
+//         $.ajax({
+//             type: 'POST',
+//             url: '/api/post/',
+//             data: order,
+//             success: function(newOrder){
+//                 addOrder(newOrder)
+//             },
+//             error: function(){
+//                 alert('error saving order')
+//             },
+//             error: function(xhr, status, error) {
+//                 var jsonResponse = JSON.parse(xhr.responseText);
+//                 if (jsonResponse.errors) {
+//                     var errorMessage = "";
+//                     $.each(jsonResponse.errors, function(key, value) {
+//                         errorMessage += value.join(", ") + "\n";
+//                     });
+//                     alert(errorMessage);
+//                 }
+//             }
+//         })
+//     });
+
+//     $orders.delegate('.remove', 'click', function(){
+
+//         var dataId = $(this).attr('data-id')
+//         var $li = $(this).closest('li');
+//         $.ajax({
+//             type: 'DELETE',
+//             url: '/api/post/',
+//             data: { id: dataId },
+//             success: function() {
+//                 $li.fadeOut(300, function(){
+//                     $('this').remove();
+//                     console.log('Data deleted successfully');
+//                 })
+//             },
+//             error: function() {
+//                 console.error('Error deleting data:');
+//             }
+
+//         });
+//     });
+
+//     $orders.delegate('.editOrder', 'click', function(){
+//         var $li = $(this).closest('li');
+//         $li.find('input.name').val( $li.find('span.name').html() );
+//         $li.find('input.drink').val( $li.find('span.drink').html() );
+//         $li.addClass('edit');
+//     })
+
+//     $orders.delegate('.cancelEdit', 'click', function(){
+//         $(this).closest('li').removeClass('edit');
+//     })
+
+//     $orders.delegate('.saveEdit', 'click', function(){
+//         var $li = $(this).closest('li');
+//         var dataId = $(this).attr('data-id')
+//         var order = {
+//             id: dataId,
+//             name: $li.find('input.name').val(),
+//             drink: $li.find('input.drink').val()
+//         };
+//         $.ajax({
+//             type: 'PUT',
+//             url: '/api/post/',
+//             data: order,
+//             success: function(newOrder) {
+//                 $li.find('span.name').html(order.name);
+//                 $li.find('span.drink').html(order.drink);
+//                 $li.removeClass('edit');
+//             },
+//             error: function() {
+//                 alert('Error updating data:', error);
+//             }
+
+//         });
+//     })
     
-});
+// });
